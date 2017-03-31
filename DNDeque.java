be7691ef6997054f3a9c52de5d4c1ccff7754a33
XinYiC7 <xchen4@stuy.edu>
2017-03-31 14:47:31 -0400

    pass compilation

--- DNDeque.java
+++ DNDeque.java
@@ -11,61 +11,113 @@ public class DNDeque<T> implements Deque<T> {
     }
 
     //return the number of elements in the deque
-    public int size();
-
-    //Retrieves, but does not remove, the first element of this deque
-    //returns null if this deque is empty.
-    public T peek();
-
-    //Retrieves, but does not remove, the first element of this deque
-    //returns null if this deque is empty.
-    public T peekFirst();
-
-    //Retrieves, but does not remove, the last element of this deque
-    //returns null if this deque is empty.
-    public T peekLast();
-
-    //adding an element at the tail of this deque
-    //returning true upon success 
-    public boolean add( T x );
-
-    //inserts the specified element at the front of the deque 
-    public void addFirst( T x );
-
-    //inserts the specified element at the end of the deque
-    public void addLast( T x );
-
-    //Retrieves and removes the first element of this deque.
-    public T remove();
-
-    //Retrieves and removes the first element of this deque.
-    public T removeFirst();
-
-    //Retrieves and removes the last element of this deque.
-    public T removeLast();
-
-    //Returns true if this deque contains the specified element.
-    public boolean contains( T x );
-
-    //Removes the first occurrence of the specified element from this deque.
-    public boolean remove( T x );
-
-    //Removes the first occurrence of the specified element from this deque.
-    public boolean removeFirstOccurrence( T x );
-
-    //Removes the last occurrence of the specified element from this deque.
-    public boolean removeLastOccurrence( T x );
-
-    //Pops an element from the stack represented by this deque.
-    public T pop();
-
-    //Pushes an element at the head of this deque
-    //returning true upon success 
-    public void push( T x );
+    public int size()
+    {
+	return _size;
+    }
 
     public boolean isEmpty()
     { 
 	return _size == 0;
+    }
+    
+    //Retrieves, but does not remove, the first element of this deque
+    //returns null if this deque is empty.
+    public T peek()
+    {
+	if (isEmpty()){
+	    return null;
+	}
+	return peekFirst();
+    }
+
+    //Retrieves, but does not remove, the first element of this deque
+    //returns null if this deque is empty.
+    public T peekFirst()
+    {
+	return _front.getCargo();
+    }
+
+    //Retrieves, but does not remove, the last element of this deque
+    //returns null if this deque is empty.
+    public T peekLast()
+    {
+	if (isEmpty()){
+	    return null;
+	}
+	return _end.getCargo();
+    }
+
+    //adding an element at the tail of this deque
+    //returning true upon success 
+    public boolean add( T x )
+    {
+	return false;
+    }
+
+    //inserts the specified element at the front of the deque 
+    public void addFirst( T x )
+    {
+    }
+
+    //inserts the specified element at the end of the deque
+    public void addLast( T x )
+    {
+	
+    }
+
+    //Retrieves and removes the first element of this deque.
+    public T remove()
+    {
+	return null;
+    }
+
+    //Retrieves and removes the first element of this deque.
+    public T removeFirst()
+    {
+	return null;
+    }
+
+    //Retrieves and removes the last element of this deque.
+    public T removeLast()
+    {
+	return null;
+    }
+
+    //Returns true if this deque contains the specified element.
+    public boolean contains( T x )
+    {
+	return false;
+    }
+
+    //Removes the first occurrence of the specified element from this deque.
+    public boolean remove( T x )
+    {
+	return false;
+    }
+
+    //Removes the first occurrence of the specified element from this deque.
+    public boolean removeFirstOccurrence( T x )
+    {
+	return false;
+    }
+
+    //Removes the last occurrence of the specified element from this deque.
+    public boolean removeLastOccurrence( T x )
+    {
+	return false;
+    }
+
+    //Pops an element from the stack represented by this deque.
+    public T pop()
+    {
+	return null;
+    }
+
+    //Pushes an element at the head of this deque
+    //returning true upon success 
+    public void push( T x )
+    {
     }
 
     // print each node
@@ -74,7 +126,7 @@ public class DNDeque<T> implements Deque<T> {
 	String retStr = "FRONT->";
 	DLLNode<T> tmp = _front; //init tr
 	while( tmp != null ) {
-	    retStr += tmp.getValue() + "->";
+	    retStr += tmp.getCargo() + "->";
 	    tmp = tmp.getNext();
 	}
 	retStr += "END";
